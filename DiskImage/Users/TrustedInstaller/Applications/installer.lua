70bab32d02196a22572fe684c0ad981d4ed48464
Willow Bracker <willow.bracker@gmail.com>
2023-07-20 23:23:05 -0400

    change installer to make screen blue, more testing basically

--- DiskImage/Users/TrustedInstaller/Applications/installer.lua
+++ DiskImage/Users/TrustedInstaller/Applications/installer.lua
@@ -1,1 +1,13 @@
-print('Testing installer...')
+print('[Rubikon]: Running Installer...')
+
+sleep()
+
+local colors    = _G.colors
+local fs        = _G.fs
+local http      = _G.http
+local install   = _ENV.install
+local os        = _G.os
+local terminal = _G.term
+
+terminal.setBackgroundColor(colors.blue)
+
